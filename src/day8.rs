1fb3f26f02defa236a8be264643eb57e335feb03
mdtro <mdtro@protonmail.com>
2021-12-09 10:36:24 -0600

    day 8; part 2 -- it's not pretty, but it works and is prettier than my first attempt without HashSet

--- src/day8.rs
+++ src/day8.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 #[aoc_generator(day8)]
 fn input_generator(input: &str) -> Vec<(String, String)> {
     input
@@ -31,11 +33,154 @@ fn part1(input: &Vec<(String, String)>) -> usize {
     // 8            7
 }
 
+#[aoc(day8, part2)]
+fn part2(input: &Vec<(String, String)>) -> i32 {
+    // DIGIT    =>  SEGMENTS ON
+    // 0            6 (contains #1, but not 9) [round 3]
+    // 1            2 (given) [round 1]
+    // 2            5 (do this one last)
+    // 3            5 (not #1) [round 2]
+    // 4            4 (given) [round 1]
+    // 5            5 (contained in 6)
+    // 6            6 (figure out 1 and 9 first) [round 4]
+    // 7            3 (given) [round 1]
+    // 8            7 (given) [round 1]
+    // 9            6 (contains #4) [round 2]
+
+    // [1, 4, 7, 8]
+    // [9]
+    // [3, 0]
+    // [6]
+    // [5]
+    // [2]
+
+    let mut total_sum = 0;
+
+    for (segments, digits) in input {
+        let mut wire_mapping_to_digit: HashMap<char, HashSet<char>> = HashMap::new();
+        let mut display: Vec<char> = vec![];
+
+        while wire_mapping_to_digit.len() < 9 {
+            //dbg!(&wire_mapping_to_digit.len());
+            //dbg!(&round);
+            //dbg!(&wire_mapping_to_digit);
+            for segment in segments.split(' ') {
+                let wire: HashSet<char> = segment.chars().collect();
+
+                // round 1
+                // find the givens: 1, 4, 7, 8
+                if segment.len() == 2 {
+                    wire_mapping_to_digit.insert('1', wire.clone());
+                } else if segment.len() == 4 {
+                    wire_mapping_to_digit.insert('4', wire.clone());
+                } else if segment.len() == 3 {
+                    wire_mapping_to_digit.insert('7', wire.clone());
+                } else if segment.len() == 7 {
+                    wire_mapping_to_digit.insert('8', wire.clone());
+                }
+
+                // round 2
+                // find 9
+                if let Some(mapping_4) = wire_mapping_to_digit.get(&'4') {
+                    if segment.len() == 6 && wire.is_superset(&mapping_4) {
+                        wire_mapping_to_digit.insert('9', wire.clone());
+                    }
+                }
+
+                // should also be round 2
+                // find 3
+                if let Some(mapping_1) = wire_mapping_to_digit.get(&'1') {
+                    if segment.len() == 5 && wire.is_superset(&mapping_1) {
+                        wire_mapping_to_digit.insert('3', wire.clone());
+                    }
+                }
+
+                // round 3
+                // find 0
+                if let (Some(mapping_9), Some(mapping_1)) = (
+                    wire_mapping_to_digit.get(&'9'),
+                    wire_mapping_to_digit.get(&'1'),
+                ) {
+                    if segment.len() == 6
+                        && !wire.is_superset(&mapping_9)
+                        && wire.is_superset(&mapping_1)
+                    {
+                        wire_mapping_to_digit.insert('0', wire.clone());
+                    }
+                }
+
+                // at this point we know 1, _ , 3, 4, _, _, 7, 8, 9
+
+                // round 4
+                // find 6
+                if let (Some(mapping_9), Some(mapping_1)) = (
+                    wire_mapping_to_digit.get(&'9'),
+                    wire_mapping_to_digit.get(&'1'),
+                ) {
+                    if segment.len() == 6
+                        && !wire.is_superset(&mapping_1)
+                        && !wire.is_superset(&mapping_9)
+                    {
+                        wire_mapping_to_digit.insert('6', wire.clone());
+                    }
+                }
+
+                // round 5?
+                // find 5
+                if let Some(mapping_6) = wire_mapping_to_digit.get(&'6') {
+                    if segment.len() == 5 && wire.is_subset(&mapping_6) {
+                        wire_mapping_to_digit.insert('5', wire.clone());
+                    }
+                }
+
+                // last round, by deduction we know 2
+
+                // if let (Some(mapping_6), Some(mapping_5), Some(mapping_3)) = (
+                //     wire_mapping_to_digit.get(&'6'),
+                //     wire_mapping_to_digit.get(&'5'),
+                //     wire_mapping_to_digit.get(&'3'),
+                // ) {
+                //     if segment.len() == 5 && wire_mapping_to_digit.len() == 9 {
+                //         wire_mapping_to_digit.insert('2', wire.clone());
+                //     }
+                // }
+            }
+        }
+
+        for digit in digits.split(' ') {
+            // this seems very inefficient...
+
+            let digit_chars: HashSet<char> = digit.chars().collect();
+
+            // find the number where all of the characters are matched
+            // and append it onto the display
+            let mut found = false;
+            for mapping in &wire_mapping_to_digit {
+                if mapping.1 == &digit_chars {
+                    // println!("{} => {:?} (num {})", &digit, &mapping.1, &mapping.0);
+                    display.push(mapping.0.clone());
+                    found = true;
+                }
+            }
+
+            if !found {
+                display.push('2');
+            }
+        }
+
+        let display_value = display.into_iter().collect::<String>();
+        total_sum += display_value.parse::<i32>().unwrap();
+    }
+
+    total_sum
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: &str = "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+    const INPUT: &str =
+        "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
 edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
 fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
 fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
@@ -50,5 +195,11 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
     fn test_part1() {
         let parsed_input = input_generator(INPUT);
         assert_eq!(26, part1(&parsed_input))
+    }
+
+    #[test]
+    fn test_part2() {
+        let parsed_input = input_generator(INPUT);
+        assert_eq!(61229, part2(&parsed_input))
     }
 }
